88d8d46dc1ec53095b034c9445dc21c5fa2b3630
kiloa44 <khaledalmansor888@gmail.com>
2019-12-28 10:37:37 +0200

    what else?

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! test test </Text>
       <Text>hello world
+        what else ?
       </Text>
     </View>
   );
