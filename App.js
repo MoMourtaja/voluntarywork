59279ee84a58de7f0356463a7c648d7f33024312
MoMourtaja <mohammedmourtaja@gmail.com>
2019-12-28 11:13:32 +0200

    jj

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
-
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+
       <Text>Open up App.js to start working on your app! test test text addedd from mo</Text>
     <Text>Hello it's me</Text>
+
+      <Text>Open up App.js to start working on your app! test test text addedd from mo</Text>
+    <Text>Hello it's me</Text>
+
       <Text>Open up App.js to start working on your app! test test </Text>
       <Text>hello world
         what else ?
       </Text>
     </View>
   );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 }
 
 const styles = StyleSheet.create({
