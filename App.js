b8ba4430323b3779ee7ba401f3956bbf583fdcd7
kiloa44 <khaledalmansor888@gmail.com>
2019-12-28 11:09:00 +0200

    login

--- App.js
+++ App.js
@@ -22,13 +22,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
