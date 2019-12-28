0d31ed5e4a8902bae495b35ba042efcb6bf4549a
Utk2019medo@@ <mohammedmourtaja@gmail.com>
2019-12-28 10:32:30 +0200

    MOuraja message

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! test test text addedd from mo</Text>
+    <Text>Hello it's me</Text>
     </View>
   );
 }
