7721031be501341402374eb328f6ebbfc7aa31ed
kiloa44 <khaledalmansor888@gmail.com>
2019-12-28 10:29:35 +0200

    hello wrold

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! test test </Text>
+      <Text>hello world
+      </Text>
     </View>
   );
 }
